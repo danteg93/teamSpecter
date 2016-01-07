f9490564c90db03f1275e8a1818013c41b91ca1b
Tyler Hogan <tybot@cox.net>
2016-01-06 23:44:14 -0800

    Simpler movement script using forces

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,48 +4,22 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
   // Use this for initialization
-  public float movementOffset = 0.5f;
-
-  private int timesDead = 0;
-
+  public float Speed = 0f;
   public float playerNumber = 1;
 
+  private int timesDead = 0;
   private Vector3 initialPosition;
+
   void Start() {
     timesDead = 0;
     initialPosition = gameObject.transform.position;
   }
 
   // Update is called once per frame
-  void Update() {
-    if (Input.GetKeyDown("w") && playerNumber == 1) {
-      gameObject.transform.Translate(Vector3.up * movementOffset);
-    }
-    if (Input.GetKeyDown("a") && playerNumber == 1) {
-      gameObject.transform.Translate(Vector3.left * movementOffset);
-    }
-    if (Input.GetKeyDown("s") && playerNumber == 1) {
-      gameObject.transform.Translate(Vector3.down * movementOffset);
-    }
-    if (Input.GetKeyDown("d") && playerNumber == 1) {
-      gameObject.transform.Translate(Vector3.right * movementOffset);
-
-    }
-
-    if (Input.GetKeyDown(KeyCode.UpArrow) && playerNumber != 1) {
-      gameObject.transform.Translate(Vector3.up * movementOffset);
-    }
-    if (Input.GetKeyDown(KeyCode.LeftArrow) && playerNumber != 1) {
-      gameObject.transform.Translate(Vector3.left * movementOffset);
-    }
-    if (Input.GetKeyDown(KeyCode.DownArrow) && playerNumber != 1) {
-      gameObject.transform.Translate(Vector3.down * movementOffset);
-    }
-    if (Input.GetKeyDown(KeyCode.RightArrow) && playerNumber != 1) {
-      gameObject.transform.Translate(Vector3.right * movementOffset);
-
-    }
+  void FixedUpdate() {
+    GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxis("Horizontal") * Speed, Input.GetAxis("Vertical") * Speed);
   }
+
   public void reSpawn() {
     timesDead++;
     gameObject.transform.position = initialPosition;
