9979c0b4dcb4d015ddc24134195f4a80a1a8edd4
Tyler Hogan <tybot@cox.net>
2016-01-07 01:24:11 -0800

    Renamed Platform.cs to make a bit more sense

--- Assets/Scripts/Gamemode.cs
+++ Assets/Scripts/Gamemode.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlatformScript : MonoBehaviour {
+public class Gamemode : MonoBehaviour {
 
   // Use this for initialization
   void Start() {
