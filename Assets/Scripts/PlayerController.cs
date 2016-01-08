2f37ab70f0330dbe8f18e286638442264a0d8d1f
Tyler Hogan <tybot@cox.net>
2016-01-07 21:41:22 -0800

    Basic attack and block (instant kill version)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,10 @@ public class PlayerController : MonoBehaviour {
   // Use this for initialization
   public float PlayerNumber = 1;
   public float Speed = 0f;
-  public float AttackPower = 0f;
 
   private int timesDead = 0;
   private Vector3 initialPosition;
-  private bool isAttacking = false;
+  private bool isPressingAttack = false;
   private bool isBlocking = false;
 
   void Start() {
@@ -24,11 +23,11 @@ public class PlayerController : MonoBehaviour {
     GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxis("Horizontal") * Speed, Input.GetAxis("Vertical") * Speed);
 
     // Perform an attack if the player is pressing an attack key
-    if (Input.GetAxis("Attack") != 0) {
+    if (Input.GetAxis("Slash") != 0) {
       // Prevent the attack from occurring multiple times in one key press and
       // the player from using multiple abilities at once (i.e. attack & block)
-      if (!isAttacking) {
-        isAttacking = true;
+      if (!isPressingAttack) {
+        isPressingAttack = true;
 
         // Find all objects in range of the player, and push other player objects away
         // TODO: This needs to only select things in front of the player
@@ -38,20 +37,20 @@ public class PlayerController : MonoBehaviour {
           if (hitColliders[i].GetComponent<PlayerController>() != null) {
             PlayerController player = hitColliders[i].GetComponent<PlayerController>();
             if (player != this && !player.IsBlocking()) {
-              player.GetComponent<Rigidbody2D>().AddForce(transform.right * AttackPower);
+              player.Kill();
             }
           }
           i++;
         }
       }
-    } else if (Input.GetAxis("Attack") == 0) {
-      isAttacking = false;
+    } else if (Input.GetAxis("Slash") == 0) {
+      isPressingAttack = false;
     }
 
     // Check if a player is trying to block
     if (Input.GetAxis("Block") != 0) {
       isBlocking = true;
-    } else if (Input.GetAxis("Block") != 0) {
+    } else if (Input.GetAxis("Block") == 0) {
       isBlocking = false;
     }
   }
