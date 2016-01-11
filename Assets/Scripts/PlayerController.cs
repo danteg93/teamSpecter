030340e134d951bf0e8615910c092683b8e1ac1c
Tyler Hogan <tybot@cox.net>
2016-01-11 01:41:41 -0800

    Abstracted attack / block to methods

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ public class PlayerController : MonoBehaviour {
 
   // Use this for initialization
   public bool KeyboardControl = true;
-  public int playerNumber;
+  public int PlayerNumber;
   public float Speed = 0f;
 
   private int timesDead = 0;
@@ -19,11 +19,7 @@ public class PlayerController : MonoBehaviour {
   }
 
   // Update is called once per frame
-  void FixedUpdate() {
-    if (playerNumber != 1) {
-      return;
-    }
- 
+  void FixedUpdate() { 
     //if player is 1 the execute the joystick movement else do keyboard and mouse
     if (KeyboardControl) {
       GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxis("HorizontalMovementK") * Speed, Input.GetAxis("VerticalMovementK") * Speed);
@@ -34,6 +30,40 @@ public class PlayerController : MonoBehaviour {
       executeJoyStickRotation();
     }
 
+    processAttackInput();
+    processBlockInput();
+  }
+
+  public bool IsBlocking() {
+    return isBlocking;
+  }
+
+  public void Kill() {
+    timesDead++;
+    transform.position = initialPosition;
+  }
+
+  private void executeMouseRotation() {
+    //Vector3 from the object to the mouse
+    Vector3 objectToMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+    objectToMouse.Normalize();
+    //Good old arctan2 lmao
+    float angleToMouse = (Mathf.Atan2(objectToMouse.y, objectToMouse.x) * Mathf.Rad2Deg) + 90.0f; //The 90 assumes that the front of the sprite is the bottom part.  
+    //Do you even quaternion bro?
+    transform.rotation = Quaternion.Euler(0f, 0f, angleToMouse);
+  }
+
+  private void executeJoyStickRotation() {
+    //Vector3 that says where the joystick is
+    Vector3 joyStickLocation = new Vector3(Input.GetAxis("HorizontalRotationJ1"), Input.GetAxis("VerticalRotationJ1"));
+    if (joyStickLocation.magnitude != 0) { //this wont reset your rotation if you randomly let go off the controller
+      joyStickLocation.Normalize();
+      float angleToStick = (Mathf.Atan2(joyStickLocation.x, joyStickLocation.y) * Mathf.Rad2Deg);
+      transform.rotation = Quaternion.Euler(0f, 0f, angleToStick);
+    }
+  }
+
+  private void processAttackInput() {
     // Perform an attack if the player is pressing an attack key
     if (Input.GetAxis("Slash") != 0) {
       // Prevent the attack from occurring multiple times in one key press and
@@ -58,7 +88,9 @@ public class PlayerController : MonoBehaviour {
     } else if (Input.GetAxis("Slash") == 0) {
       isPressingAttack = false;
     }
+  }
 
+  private void processBlockInput() {
     // Check if a player is trying to block
     if (Input.GetAxis("Block") != 0) {
       isBlocking = true;
@@ -66,35 +98,5 @@ public class PlayerController : MonoBehaviour {
     } else if (Input.GetAxis("Block") == 0) {
       isBlocking = false;
     }
-  }
-
-  private void executeMouseRotation() {
-    //Vector3 from the object to the mouse
-    Vector3 objectToMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-    objectToMouse.Normalize();
-    //Good old arctan2 lmao
-    float angleToMouse = (Mathf.Atan2(objectToMouse.y, objectToMouse.x) * Mathf.Rad2Deg) + 90.0f; //The 90 assumes that the front of the sprite is the bottom part.  
-    //Do you even quaternion bro?
-    transform.rotation = Quaternion.Euler(0f, 0f, angleToMouse);
-  }
-
-  private void executeJoyStickRotation() {
-    //Vector3 that says where the joystick is
-    Vector3 joyStickLocation = new Vector3(Input.GetAxis("HorizontalRotationJ1"), Input.GetAxis("VerticalRotationJ1"));
-    if (joyStickLocation.magnitude != 0){ //this wont reset your rotation if you randomly let go off the controller
-      joyStickLocation.Normalize();
-      float angleToStick = (Mathf.Atan2(joyStickLocation.x, joyStickLocation.y) * Mathf.Rad2Deg);
-      transform.rotation = Quaternion.Euler(0f, 0f, angleToStick);
-    }
-  }
-
-  public bool IsBlocking() {
-    return isBlocking;
-  }
-
-  public void Kill() {
-    timesDead++;
-    GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-    transform.position = initialPosition;
   }
 }
