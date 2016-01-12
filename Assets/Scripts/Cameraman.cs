3d28439c2e0ee9fb58c269409d8c50a8d5a5c1e2
xiarongx <xiarongx@uci.edu>
2016-01-11 16:54:30 -0800

    shake shake shake
    call cameraShake(float shakeDuration, float shakePower)
    to shake

---
+++ Assets/Scripts/Cameraman.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cameraman : MonoBehaviour {
+
+    public float shakeTimer;
+    public float shakeAmount;
+
+  // Use this for initialization
+  void Start()
+  {
+    transform.position = new Vector3(0,0,-10);
+  }
+
+  // Update is called once per frame
+  void Update () {
+	  if (shakeTimer >= 0)
+      {
+        Vector2 ShakePos = Random.insideUnitCircle * shakeAmount;
+        transform.position = new Vector3(transform.position.x + ShakePos.x, transform.position.y, transform.position.z);
+        shakeTimer -= Time.deltaTime;
+
+      }
+
+    if (Input.GetKey("="))
+    {
+      cameraShake(1, 0.1f);
+    }
+    
+	}
+
+  public void cameraShake(float shakeDuration, float shakePower)
+  {
+    shakeAmount = shakePower;
+    shakeTimer = shakeDuration;
+  }
+}
