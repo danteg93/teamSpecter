4e216a1b7a5c92ff7f6b374b9b37c22bf61cf758
Charles Pasiliao <Charles.Pasiliao@gmail.com>
2016-01-11 21:19:19 -0800

    Bullet projectile object added. Gave it a temporary sprite and destroys player tag game object. Bullet destroys itself after 2 sec if it doesnt colide with anything

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour {
+
+	public float FireRate = 0; 
+	public LayerMask NotToHit;
+
+	float timeToFire = 0;
+	Transform firePoint;
+	// Use this for initialization
+	void Start () {
+		firePoint = transform.FindChild ("FirePoint");
+		if (firePoint == null) {
+			Debug.LogError ("Something is wrong!");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (FireRate == 0) {
+			if (Input.GetMouseButtonDown (0)) {
+				Shoot ();
+			}
+		} 
+		else {
+			if (Input.GetMouseButtonDown (0) && Time.time > timeToFire) {
+				timeToFire = Time.time + 1 / FireRate;
+				Shoot ();
+			}
+		}
+	}
+
+	void Shoot (){
+		Debug.Log ("Test");
+		Vector2 mousePosition = new Vector2 (Camera.main.ScreenToWorldPoint (Input.mousePosition).x, Camera.main.ScreenToWorldPoint (Input.mousePosition).y);
+		Vector2 firePointPosition = new Vector2 (firePoint.position.x, firePoint.position.y);
+		RaycastHit2D hit = Physics2D.Raycast (firePointPosition, mousePosition - firePointPosition, 100, NotToHit);
+		Debug.DrawLine (firePointPosition, mousePosition);
+	}
+}
---
+++ Assets/Scripts/projectile.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class projectile : MonoBehaviour {
+	public float Velocity = 1;
+	private float selfDestroyTimer = 1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 position = this.transform.position;
+		position.y--;
+		this.transform.position = position;
+
+		selfDestroyTimer -= Time.deltaTime;
+
+		if (selfDestroyTimer <= 0) {
+			Destroy (gameObject);
+		}
+		}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if(col.gameObject.tag == "Player")
+		{
+			Debug.Log ("destroyed player");
+			Destroy(col.gameObject);
+			Destroy (gameObject);
+		}
+	}
+
+
+}
