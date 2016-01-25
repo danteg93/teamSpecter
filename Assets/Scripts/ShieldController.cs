646c5e9388fbfaaa517887bb55b783d9431f80a9
Dante <dante@oomba.com>
2016-01-25 09:28:33 -0800

    added a textured sphere that appears when the player presses the block button.

---
+++ Assets/Scripts/ShieldController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShieldController : MonoBehaviour {
+
+  private bool shieldEnabled = false;
+  private MeshRenderer renderer;
+  // Update is called once per frame
+  void Start() {
+    renderer = GetComponent<MeshRenderer>();
+  }
+  void Update() {
+    if (shieldEnabled) {
+      renderer.enabled = true;
+      transform.Rotate(1.0f, 0.0f, 1.0f);
+    }
+    else {
+      renderer.enabled = false;
+    }
+  }
+
+  public void ActivateShield() {
+    if (!shieldEnabled) {
+      shieldEnabled = true;
+    }
+  }
+  public void DeactivateShield() {
+    if (shieldEnabled) {
+      shieldEnabled = false;
+    }
+  }
+}
