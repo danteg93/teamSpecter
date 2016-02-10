ea87727e6079d24ef7f0dfa133e0c2d2b7abc88c
Tyler Hogan <tybot@cox.net>
2016-02-09 20:01:22 -0800

    Checkpoint for game over

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+  GameObject mainMenuGUI;
+  GameObject gameOverGUI;
+
+  void Start() {
+    mainMenuGUI = GameObject.FindGameObjectWithTag("MainMenuGUI");
+    gameOverGUI = GameObject.FindGameObjectWithTag("GameOverGUI");
+  }
+
+  public void LoadScene(int sceneNumber) {
+    SceneManager.LoadScene(sceneNumber);
+  }
+
+  public void Quit() {
+    Application.Quit();
+  }
+}
