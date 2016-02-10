b873596eb91fdc2999bebc9489050ab641748fed
Tyler Hogan <tybot@cox.net>
2016-02-09 21:09:07 -0800

    Added start menu; Cleaned up menu code

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -4,12 +4,31 @@ using System.Collections;
 
 public class MenuController : MonoBehaviour {
 
-  GameObject mainMenuGUI;
-  GameObject gameOverGUI;
+  enum Page { Start, Main, GameOver }
+
+  private Page currentPage = Page.Start;
+  private GameObject startMenuGUI;
+  private GameObject mainMenuGUI;
+  private GameObject gameOverGUI;
 
   void Start() {
+    startMenuGUI = GameObject.FindGameObjectWithTag("StartMenuGUI");
     mainMenuGUI = GameObject.FindGameObjectWithTag("MainMenuGUI");
     gameOverGUI = GameObject.FindGameObjectWithTag("GameOverGUI");
+    mainMenuGUI.SetActive(false);
+    gameOverGUI.SetActive(false);
+  }
+
+  void OnGUI() {
+    switch (currentPage) {
+      case Page.Start: showStartMenu(); break;
+      case Page.Main: showMainMenu(); break;
+      case Page.GameOver: showGameOver(); break;
+    }
+  }
+
+  public void StartGame() {
+    currentPage = Page.Main;
   }
 
   public void LoadScene(int sceneNumber) {
@@ -18,5 +37,23 @@ public class MenuController : MonoBehaviour {
 
   public void Quit() {
     Application.Quit();
+  }
+
+  private void showStartMenu() {
+    startMenuGUI.SetActive(true);
+    mainMenuGUI.SetActive(false);
+    gameOverGUI.SetActive(false);
+  }
+
+  private void showMainMenu() {
+    startMenuGUI.SetActive(false);
+    mainMenuGUI.SetActive(true);
+    gameOverGUI.SetActive(false);
+  }
+
+  private void showGameOver() {
+    startMenuGUI.SetActive(false);
+    mainMenuGUI.SetActive(false);
+    gameOverGUI.SetActive(true);
   }
 }
