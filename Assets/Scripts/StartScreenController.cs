1288a8280a0ed8a15291446423691b692e5247b3
Dante <dante@oomba.com>
2016-02-25 21:31:38 -0800

    added option to display mouse on start screen

--- Assets/Scripts/StartScreenController.cs
+++ Assets/Scripts/StartScreenController.cs
@@ -3,7 +3,12 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class StartScreenController : MonoBehaviour {
-  
+
+  public bool DisplayMouse = true;
+
+  void Start() {
+    Cursor.visible = DisplayMouse;
+  }
   //This was created because it would have been weird to pass parameters
   //to the menu manager for it to know whether it had been previously initiliazed after a game had been played
 	void Update () {
