73a4e8799b705ef99f5baa7a0248e721ed4aaa58
Dante <dante@oomba.com>
2016-02-26 09:08:20 -0800

    start screen usable as a gui
    opens menu for now

--- Assets/Scripts/Menu Scripts/StartScreenController.cs
+++ Assets/Scripts/Menu Scripts/StartScreenController.cs
@@ -11,9 +11,7 @@ public class StartScreenController : MonoBehaviour {
   }
   //This was created because it would have been weird to pass parameters
   //to the menu manager for it to know whether it had been previously initiliazed after a game had been played
-	void Update () {
-    if (Input.GetAxis("SUBMIT") != 0) {
-      SceneManager.LoadScene(1);
-    }
-	}
+  public void openMenu() {
+    SceneManager.LoadScene(2);
+  }
 }
