31d86f25905d6bdc69fdb865238361ff0abf6cc8
Dante <dante@oomba.com>
2016-03-08 05:42:41 -0800

    Added start screen with players!
    made it look like our cover (players can aim)

--- Assets/Scripts/Menu Scripts/StartScreenController.cs
+++ Assets/Scripts/Menu Scripts/StartScreenController.cs
@@ -8,7 +8,21 @@ public class StartScreenController : MonoBehaviour {
   //to the menu manager for it to know whether it had been previously initiliazed after a game had been played
   //We might also want to add more stuff between the start screen and the game set up.
   public bool DisplayMouse = true;
+  private PlayerController[] players = new PlayerController[4];
 
+  void Awake() {
+    PlayerController[] tempPlayers = FindObjectsOfType(typeof(PlayerController)) as PlayerController[];
+    for (int i = 0; i < tempPlayers.Length; i++) {
+      tempPlayers[i].setInitializedByGamemode(true);
+      players[tempPlayers[i].PlayerNumber - 1] = tempPlayers[i];
+    }
+    for (int i = 0; i < players.Length; i++) {
+      players[i].SetPlayerInvincibility(true);
+    }
+    for (int i = 0; i < players.Length; i++) {
+      players[i].SetPlayerMoveAndShoot(false);
+    }
+  }
   void Start() {
     Cursor.visible = DisplayMouse;
   }
